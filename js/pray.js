8a0c2ce735182c41032a56b52a3fc0e1915e30ad
jmcob <jmcob@pm.me>
2021-11-16 13:47:14 +0100

    transition et changements de couleurs

--- js/pray.js
+++ js/pray.js
@@ -1,20 +1,23 @@
 const time = document.getElementById("itemQuantity");
 const minuteTimer = time.value * 60000; //minuteTimer = time en millisecondes
+// const minuteTimer = 3000;
+
 const remainingTime = document.getElementById("remainingTime");
 const button = document.getElementById("button");
 button.addEventListener("click", () => {
+  setTimeout(alertFunc, minuteTimer);
+  document.body.style.background = "midnightblue";
   remainingTime.innerHTML =
     "Vous avez choisi de prier " +
     time.value +
-    " minute(s) à " +
+    " minute(s). <br><br>le " +
     new Date() +
-    ". Ce mesage disparaitra au terme du temps imparti !";
+    ".<br><br> Ce message disparaitra au terme du temps imparti !";
 });
-function setTime() {
-  setTimeout(alertFunc, minuteTimer);
-}
+
 function alertFunc() {
   remainingTime.style.color = "orangered";
+  document.body.style.background = "mistyrose";
   remainingTime.innerHTML =
     "Terminé! Vous avez prié " + time.value + " minute(s)";
 }
