ec17695b3d5ea9d5a058b4151427f0c279adadff
jmcob <jmcob@pm.me>
2021-12-13 15:12:20 +0100

    small update about time

--- js/pray.js
+++ js/pray.js
@@ -5,44 +5,20 @@ const minuteTimer = time.value * 60000; //minuteTimer = time en millisecondes
 const remainingTime = document.getElementById("remainingTime");
 const button = document.getElementById("button");
 button.addEventListener("click", () => {
-  setTimeout(alertFunc, minuteTimer);
-  document.body.style.background = "midnightblue";
-  remainingTime.innerHTML =
-    "Vous avez choisi de prier " +
-    time.value +
-    " minute(s). <br><br>le " +
-    new Date() +
-    ".<br><br> Ce message disparaitra au terme du temps imparti !";
+        setTimeout(alertFunc, minuteTimer);
+        document.body.style.background = "midnightblue";
+        remainingTime.innerHTML =
+                "Vous avez choisi de prier " +
+                time.value +
+                " minute(s). <br><br>le " +
+                new Date().toLocaleTimeString +
+                ".<br><br> Ce message disparaitra au terme du temps imparti !";
 });
 
 function alertFunc() {
-  remainingTime.style.color = "orangered";
-  document.body.style.background = "mistyrose";
-  remainingTime.innerHTML =
-    "Terminé! Vous avez prié " + time.value + " minute(s)";
-  new Audio("hey.ogg").play();
+        remainingTime.style.color = "orangered";
+        document.body.style.background = "mistyrose";
+        remainingTime.innerHTML =
+                "Terminé! Vous avez prié " + time.value + " minute(s)";
+        new Audio("hey.ogg").play();
 }
-
-// // The wake lock sentinel.
-// var wakeLock = null;
-
-// // Function that attempts to request a screen wake lock.
-// const requestWakeLock = async () => {
-//   try {
-//     wakeLock = await navigator.wakeLock.request();
-//     wakeLock.addEventListener("release", () => {
-//       console.log("Screen Wake Lock released:", wakeLock.released);
-//     });
-//     console.log("Screen Wake Lock released:", wakeLock.released);
-//   } catch (err) {
-//     console.error(`${err.name}, ${err.message}`);
-//   }
-// };
-
-// // Request a screen wake lock…
-// // await requestWakeLock();
-// // // …and release it again after 5s.
-// window.setTimeout(() => {
-//   wakeLock.release();
-//   wakeLock = null;
-// }, 5000);
