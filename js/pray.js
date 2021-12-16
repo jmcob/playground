1762b4cb3ff976dd40b9aa456124601e42acce14
jmcob <jmcob@pm.me>
2021-12-16 16:34:44 +0100

    pray chronometer

--- js/pray.js
+++ js/pray.js
@@ -1,31 +1,36 @@
 const time = document.getElementById("itemQuantity");
 const remainingTime = document.getElementById("remainingTime");
+const chosenTime = document.getElementById("chosenTime");
 const button = document.getElementById("button");
 const form = document.getElementById("form");
 
 button.addEventListener("click", () => {
         let minuteTimer = time.value * 60000; //minuteTimer = time in ms
+        let ms = 0;
+        let seconds = 0;
+        let minutes = 0;
 
         // for the timer
-        setInterval(() => {
-                minuteTimer -= 60000;
-        }, 60000);
+        // setInterval(() => {
+        //         ms += 1000;
+        //         seconds = ms / 1000;
+        //         if ((seconds = 60)) {
+        //                 minutes++;
+        //                 seconds = 0;
+        //         }
+        // }, 1000);
 
         // message :
-        remainingTime.innerHTML =
-                "Vous avez choisi de prier " +
-                time.value +
-                " minute(s). <br><br> Il reste " +
-                minuteTimer / 60000 +
-                " minutes !";
+        chosenTime.innerHTML =
+                "Vous avez choisi de prier " + time.value + " minute(s).";
 
         // aesthetics and final function
         setTimeout(alertFunc, minuteTimer);
         document.body.style.background = "midnightblue";
+        document.body.style.color = "orangered";
         form.style.display = "none";
 });
 function alertFunc() {
-        document.body.style.color = "orangered";
         document.body.style.background = "green";
         remainingTime.innerHTML =
                 "Terminé! Vous avez prié " + time.value + " minute(s)";
