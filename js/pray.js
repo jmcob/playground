38522e5fcdd875b78c174ebb169e9687e88e7c75
jmcob <jmcob@pm.me>
2021-12-16 15:17:42 +0100

    update about pray.js

--- js/pray.js
+++ js/pray.js
@@ -1,23 +1,32 @@
 const time = document.getElementById("itemQuantity");
-const minuteTimer = time.value * 60000; //minuteTimer = time en millisecondes
-// const minuteTimer = 3000;
-
 const remainingTime = document.getElementById("remainingTime");
 const button = document.getElementById("button");
+const form = document.getElementById("form");
+
 button.addEventListener("click", () => {
-        setTimeout(alertFunc, minuteTimer);
-        document.body.style.background = "midnightblue";
+        let minuteTimer = time.value * 60000; //minuteTimer = time in ms
+
+        // for the timer
+        setInterval(() => {
+                minuteTimer -= 60000;
+        }, 60000);
+
+        // message :
         remainingTime.innerHTML =
                 "Vous avez choisi de prier " +
                 time.value +
-                " minute(s). <br><br>le " +
-                new Date().toLocaleTimeString +
-                ".<br><br> Ce message disparaitra au terme du temps imparti !";
-});
+                " minute(s). <br><br> Il reste " +
+                minuteTimer / 60000 +
+                " minutes !";
 
+        // aesthetics and final function
+        setTimeout(alertFunc, minuteTimer);
+        document.body.style.background = "midnightblue";
+        form.style.display = "none";
+});
 function alertFunc() {
-        remainingTime.style.color = "orangered";
-        document.body.style.background = "mistyrose";
+        document.body.style.color = "orangered";
+        document.body.style.background = "green";
         remainingTime.innerHTML =
                 "Terminé! Vous avez prié " + time.value + " minute(s)";
         new Audio("hey.ogg").play();
